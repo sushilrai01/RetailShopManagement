b467b59de58225d2b6b6de363c44f2d83f3a4ac0
sushilrai01 <ssilray16@gmail.com>
2026-02-28 15:26:25 +0545

    product list page/DI service

---
+++ RetailShopManagement.Application/Services/Products/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using RetailShopManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailShopManagement.Application.Services.Products
+{
+    public interface IProductService
+    {
+        Task<IList<Product>> GetAllProductsAsync();
+    }
+}
