3e66061824d961987492103ad9fb8b911ac6b102
sushilrai01 <ssilray16@gmail.com>
2026-02-28 21:22:35 +0545

    add connectionString/ sql server provider/ run Initial migration

---
+++ RetailShopManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RetailShopManagement.Domain.Entities;
+
+namespace RetailShopManagement.Infrastructure.Persistence
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        
+    }
+}
