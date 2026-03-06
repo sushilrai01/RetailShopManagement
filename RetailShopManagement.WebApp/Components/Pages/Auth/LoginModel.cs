5f04f1805f9d37702ea8f6934e4b38e2a82bc37e
sushilrai01 <ssilray16@gmail.com>
2026-03-06 21:20:33 +0545

    login page ui

---
+++ RetailShopManagement.WebApp/Components/Pages/Auth/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RetailShopManagement.WebApp.Components.Pages.Auth
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
