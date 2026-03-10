2691dbd5dd0dfa85eba7092792685feb95e31465
sushilrai01 <ssilray16@gmail.com>
2026-03-10 22:27:54 +0545

    Implement user login CQRS, AuthController, and UI updates
    Added LoginUserCommand and handler for CQRS-based user authentication. Refactored LoginResponseModel to include user details and removed token fields. Introduced AuthController with login endpoint using MediatR and cookie authentication. Added AccessDenied.razor for unauthorized access handling. Removed auto-navigation to login in Home.razor.

--- RetailShopManagement.Application/Common/Models/LoginResponseModel.cs
+++ RetailShopManagement.Application/Common/Models/LoginResponseModel.cs
@@ -2,9 +2,16 @@
 {
     public class LoginResponseModel
     {
-        public string? Username { get; set; }
-        public string? AccessToken { get; set; }   
-        public string? RefreshToken { get; set; }   
-        public int ExpiresIn { get; set; }   
+        public Guid Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = null!;
+        public string MobileNo { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public string Role { get; set; }
+
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
     }
 }
