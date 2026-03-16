c39d91d56ab10d6954323d98a7b27ae38b1ef75d
sushilrai01 <ssilray16@gmail.com>
2026-03-16 18:44:50 +0545

    Dialog component/ edit-verify user dialog ui

---
+++ RetailShopManagement.Domain/Constants/UserRolesConst.cs
@@ -0,0 +1,24 @@
+﻿using RetailShopManagement.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailShopManagement.Domain.Constants
+{
+    public static class UserRolesConst
+    {
+        public static string User => "User";
+        public static string Admin => "Admin";
+
+
+        // DROPDOWN LIST
+        public static IList<DropDownField> RolesFields =>
+            new List<DropDownField>
+            {
+                new() { Value = User, Text = User },
+                new() { Value = Admin, Text = Admin },
+            };
+    }
+}
