04535526684777242e8d5900028b5459e30771e7
sushilrai01 <ssilray16@gmail.com>
2026-03-17 18:16:48 +0545

    Creditor List/ payslip view accordion - Added creditorDto / invoiceDto models - Added creditor list UI with dummy data

---
+++ RetailShopManagement.Application/Common/Models/CreditorDto.cs
@@ -0,0 +1,43 @@
+﻿using RetailShopManagement.Domain.Abstractions;
+using RetailShopManagement.Domain.Constants;
+using RetailShopManagement.Domain.Entities;
+
+namespace RetailShopManagement.Application.Common.Models;
+
+public class CreditorDto
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public string MobileNo { get; set; } = null!;
+    public string? Email { get; set; }
+    public string Address { get; set; } = null!;
+
+    //Remaining amount to be paid to the creditor
+    public decimal DueAmount { get; set; } 
+
+    //Total amount paid to the creditor (total sum of PaySlips' AmountPaid)
+    public decimal PaidAmount { get; set; } 
+
+    //Total amount owed to the creditor (total sum of invoices' TotalAmount)
+    public decimal TotalAmount { get; set; } 
+
+    public string Status { get; set; } = PaymentStatus.Pending;
+
+    public DateTime CreatedOn { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
+    public DateTime? LastModifiedOn { get; set; }
+    public string LastModifiedBy { get; set; } = string.Empty;
+
+    //List of payment made to this creditor
+    public IList<PaySlipDto> PaySlips { get; set; } = new List<PaySlipDto>();
+    public IList<InvoicesDto> InvoiceLists { get; set; } = new List<InvoicesDto>();
+
+}
+public class PaySlipDto 
+{
+    public Guid Id { get; set; }
+    //public Guid CreditorId { get; set; }
+    public DateTime PaymentDate { get; set; } = DateTime.Now;
+    public decimal AmountPaid { get; set; }
+    public string Remarks { get; set; } = null!;
+}
---
+++ RetailShopManagement.Application/Common/Models/InvoicesDto.cs
@@ -0,0 +1,73 @@
+﻿using RetailShopManagement.Domain.Abstractions;
+using RetailShopManagement.Domain.Constants;
+using RetailShopManagement.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetailShopManagement.Application.Common.Models;
+
+public class InvoicesDto
+{
+    public Guid Id { get; set; }
+    public string InvoiceNumber { get; set; } = string.Empty;
+
+    public DateTime InvoiceDate { get; set; } = DateTime.Now;
+
+    public DateTime? DueDate { get; set; }
+
+    public Guid? CreditorId { get; set; }
+
+    public decimal SubTotal { get; set; }
+     
+    public decimal TaxRate { get; set; }
+    public decimal TaxAmount { get; set; }
+     
+    public decimal DiscountAmount { get; set; }
+
+    public decimal TotalAmount { get; set; }
+     
+    public decimal PaidAmount { get; set; }
+     
+    public decimal BalanceAmount { get; set; }
+
+    [Required] public string Status { get; set; } = PaymentStatus.Pending;
+
+    public string? Remarks { get; set; }
+
+    public Guid? SupplierId { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
+    public DateTime? LastModifiedOn { get; set; }
+    public string LastModifiedBy { get; set; } = string.Empty;
+
+    //List of Invoice Items
+    public IList<ProductSalesDto> InvoiceItems { get; set; } = new List<ProductSalesDto>();
+
+}
+public class ProductSalesDto 
+{
+    public Guid Id { get; set; }
+    public Guid InvoiceId { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+
+    public string? ProductCode { get; set; }
+
+    public decimal Quantity { get; set; }
+
+    public string Unit { get; set; } = null!; 
+
+    public decimal UnitPrice { get; set; }
+
+    public decimal Subtotal { get; set; }
+
+    public decimal TaxRate { get; set; } // Tax percentage (e.g., 13.00 for 13%)
+
+    public decimal TaxAmount { get; set; }
+
+    public decimal DiscountAmount { get; set; }
+     
+    public decimal TotalAmount { get; set; } // Subtotal + Tax - Discount
+    public string? Notes { get; set; }
+}
