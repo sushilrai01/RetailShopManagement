48ccb1d1db5fff1cffdf16e08729ed2d78183187
sushilrai01 <ssilray16@gmail.com>
2026-03-17 21:35:24 +0545

    Creditor: GetListQuery / CreateCommand

---
+++ RetailShopManagement.Application/CQRS/Creditors/Command/CreateCreditorCommand.cs
@@ -0,0 +1,66 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RetailShopManagement.Application.Persistence;
+using RetailShopManagement.Domain.Constants;
+using RetailShopManagement.Domain.Entities;
+using RetailShopManagement.Domain.Shared;
+
+namespace RetailShopManagement.Application.CQRS.Creditors.Command
+{
+    public class CreateCreditorCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string MobileNo { get; set; } = null!;
+        public string? Email { get; set; }
+        public string Address { get; set; } = null!;
+
+        public decimal DueAmount { get; set; }
+
+        public decimal PaidAmount { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string Status { get; set; } = PaymentStatus.Pending;
+    }
+
+    public class CreateCreditorCommandHandler(IDbContextFactory<ApplicationDbContext> contextFactory,
+        IUserServiceProvider userServiceProvider)
+        : IRequestHandler<CreateCreditorCommand, Guid>
+    {
+        public async Task<Guid> Handle(CreateCreditorCommand request, CancellationToken cancellationToken)
+        {
+            await using var context = await contextFactory.CreateDbContextAsync(cancellationToken);
+
+            var existingCreditor = await context.Creditors
+                .Where(x => x.MobileNo.Trim().ToLower() == request.MobileNo.Trim().ToLower()).
+                FirstOrDefaultAsync(cancellationToken);
+
+            if (existingCreditor != null)
+            {
+                throw new Exception("Creditor with the same mobile no. already exists.");
+            }
+
+            var creditor = new Creditor()
+            {
+                Id = Guid.NewGuid(),
+                FullName = request.FullName,
+                MobileNo = request.MobileNo,
+                Email = request.Email,
+                Address = request.Address,
+
+                DueAmount = request.DueAmount,
+                PaidAmount = request.PaidAmount,
+                BalanceAmount = request.TotalAmount,
+                Status = request.Status,
+                
+                CreatedBy = userServiceProvider.UserName,
+                CreatedOn = DateTime.Now
+            };
+            await context.Creditors.AddAsync(creditor, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return creditor.Id;
+        }
+    }
+}
---
+++ RetailShopManagement.Application/CQRS/Creditors/Query/GetCreditorsListQuery.cs
@@ -0,0 +1,94 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RetailShopManagement.Application.Common.Models;
+using RetailShopManagement.Application.Persistence;
+
+namespace RetailShopManagement.Application.CQRS.Creditors.Query
+{
+    public class GetCreditorsListQuery : IRequest<IList<CreditorDto>>
+
+    {
+
+    }
+
+    public class GetCreditorsListQueryHandler(IDbContextFactory<ApplicationDbContext> contextFactory)
+        : IRequestHandler<GetCreditorsListQuery, IList<CreditorDto>>
+    {
+        public async Task<IList<CreditorDto>> Handle(GetCreditorsListQuery request, CancellationToken cancellationToken)
+        {
+            await using var context = await contextFactory.CreateDbContextAsync(cancellationToken);
+
+            var creditorsList = await context.Creditors
+                .Include(x => x.PaySlips)
+                .Include(x => x.Invoices)
+                .AsNoTracking()
+                .Select(x => new CreditorDto()
+                {
+                    Id = x.Id,
+                    FullName = x.FullName,
+                    MobileNo = x.MobileNo,
+                    Email = x.Email,
+                    Address = x.Address,
+                    Status = x.Status,
+
+                    TotalAmount = x.Invoices.Sum(y => (decimal?)y.BalanceAmount) ?? 0,
+                    PaidAmount = x.PaySlips.Sum(y => (decimal?)y.AmountPaid) ?? 0,
+                    DueAmount = (x.Invoices.Sum(y => (decimal?)y.BalanceAmount) ?? 0)
+                                - (x.PaySlips.Sum(y => (decimal?)y.AmountPaid) ?? 0),
+
+                    CreatedOn = x.CreatedOn,
+                    CreatedBy = x.CreatedBy,
+                    LastModifiedOn = x.LastModifiedOn,
+                    LastModifiedBy = x.LastModifiedBy ?? string.Empty,
+
+                    PaySlips = x.PaySlips.Select(y => new PaySlipDto()
+                    {
+                        Id = y.Id,
+                        AmountPaid = y.AmountPaid,
+                        PaymentDate = y.PaymentDate,
+                        Remarks = y.Remarks
+                    }).ToList(),
+
+                    //TODO: Incomplete mapping of InvoiceItems; separated query
+                    InvoiceLists = x.Invoices.Select(y => new InvoicesDto() 
+                    {
+                        Id = y.Id,
+                        InvoiceNumber = y.InvoiceNumber,
+                        InvoiceDate = y.InvoiceDate,
+                        DueDate = y.DueDate,
+                        CreditorId = y.CreditorId,
+                        SubTotal = y.SubTotal,
+                        TaxAmount = y.TaxAmount,
+                        DiscountAmount = y.DiscountAmount,
+                        TotalAmount = y.TotalAmount,
+                        PaidAmount = y.PaidAmount,
+                        BalanceAmount = y.BalanceAmount,
+
+                        Status = y.Status,
+                        Remarks = y.Remarks,
+                        SupplierId = y.SupplierId,
+
+                        CreatedOn = y.CreatedOn,
+                        CreatedBy = y.CreatedBy,
+                        LastModifiedOn = y.LastModifiedOn,
+                        LastModifiedBy = y.LastModifiedBy ?? string.Empty,
+
+                        //InvoiceItems = y.InvoiceItems.Select(z => new ProductSalesDto()
+                        //{
+                        //    Id = z.Id,
+                        //    ProductId = z.ProductId,
+                        //    ProductName = z.ProductName,
+                        //    Quantity = z.Quantity,
+                        //    UnitPrice = z.UnitPrice,
+                        //    Subtotal = z.Subtotal
+                        //}).ToList()
+
+                    }).ToList()
+
+                }).OrderByDescending(x => x.CreatedOn)
+                .ToListAsync(cancellationToken);
+
+            return creditorsList;
+        }
+    }
+}
