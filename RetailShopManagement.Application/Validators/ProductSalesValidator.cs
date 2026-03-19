2beb89fd0186f1e253462bfa04367f7f31dde6f2
sushilrai01 <ssilray16@gmail.com>
2026-03-19 12:11:20 +0545

    Added fluent validation

---
+++ RetailShopManagement.Application/Validators/ProductSalesValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using RetailShopManagement.Application.Common.Models;
+
+namespace RetailShopManagement.Application.Validators
+{
+    public class ProductSalesValidator : AbstractValidator<ProductSalesDto>
+    {
+        public ProductSalesValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .NotEmpty().WithMessage("Product ID is required.");
+
+            RuleFor(x => x.ProductName)
+                .NotEmpty().WithMessage("Product Name is required.")
+                .MaximumLength(200).WithMessage("Product Name cannot exceed 200 characters.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+
+            RuleFor(x => x.Unit)
+                .NotEmpty().WithMessage("Unit is required.")
+                .MaximumLength(50).WithMessage("Unit cannot exceed 50 characters.");
+
+            //RuleFor(x => x.InvoiceId)
+            //    .NotEmpty().WithMessage("Invoice ID is required.");
+        }
+    }
+}
